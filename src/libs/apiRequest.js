deb3ccc9313933ce5c155d2ea63b4f361b01ffd5
Po Bidau <pobidau@protonmail.com>
2026-06-01 00:33:00 +0200

    fix probleme git

--- src/libs/apiRequest.js
+++ src/libs/apiRequest.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const apiRequest = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // ex: http://localhost:5000/api
+  baseURL: import.meta.env.DEV // ex: http://localhost:5000/api
+    ? import.meta.env.VITE_API_URL // ex: http://localhost:5000/api
+    : '/api/proxy',
+
   headers: {
     'Content-Type': 'application/json',
   },
