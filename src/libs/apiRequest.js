1c1f191c7272d2572ac223746eb76eb5e5892bbf
Po Bidau <pobidau@protonmail.com>
2026-05-31 23:41:04 +0200

    refactors: configuration apiResquest

--- src/libs/apiRequest.js
+++ src/libs/apiRequest.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const isDev = import.meta.env.MODE === 'development';
+const isDev = import.meta.env.MODE;
 
 const apiRequest = axios.create({
   baseURL: isDev
     ? import.meta.env.VITE_API_URL // ex: http://localhost:5000/api
-    : '/.netlify/functions/liturgieApi',
+    : '/api/proxy',
 
   headers: {
     'Content-Type': 'application/json',
