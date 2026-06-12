eb8ee0a0b749819265ca753973f98b9d0647af52
Po Bidau <pobidau@protonmail.com>
2026-06-12 22:48:53 +0200

    style: header glass

--- src/components/layouts/header/HeaderBar.jsx
+++ src/components/layouts/header/HeaderBar.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './headerBar.css';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 
 function HeaderBar() {
   const location = useLocation();
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let isScrolled = window.scrollY > 10;
+      setScrolled(isScrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up: quand le composant est détruit, on enlève le listener pour éviter fuite de mémoire
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   const titles = {
     '/': '🏠 Accueil',
@@ -14,7 +29,7 @@ function HeaderBar() {
   };
 
   return (
-    <header className="page-header">
+    <header className={`page-header ${scrolled ? 'page-header--glass' : ''}`}>
       <h1>{titles[location.pathname] || 'Page'}</h1>
 
       <button>+ Nouvelle liturgie</button>
