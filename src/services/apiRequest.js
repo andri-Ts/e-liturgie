6ab1df2b7e3b6f8f68ef68315259eabde9953631
andri-Ts <andri.ticy@ikmail.com>
2026-06-13 23:58:12 +0200

    Update apiRequest.js
    apiRequest en  mode normal

--- src/services/apiRequest.js
+++ src/services/apiRequest.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 const apiRequest = axios.create({
-  baseURL: import.meta.env.DEV ? import.meta.env.VITE_API_URL : '/api/',
-
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
+console.log(import.meta.env.VITE_API_URL);
 
 export default apiRequest;
