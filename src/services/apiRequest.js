7b633424df230b7521fb6df409122d2907e6e832
andri-Ts <andri.ticy@ikmail.com>
2026-06-13 12:26:40 +0200

    Update apiRequest.js pour prod
    apiRequest pour prod

--- src/services/apiRequest.js
+++ src/services/apiRequest.js
@@ -1,15 +1,11 @@
 import axios from 'axios';
 
-// console.log('MODE =', import.meta.env.MODE);
-// console.log('VITE_API_URL =', import.meta.env.VITE_API_URL);
-
 const apiRequest = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.DEV ? import.meta.env.VITE_API_URL : '/api/',
+
   headers: {
     'Content-Type': 'application/json',
   },
 });
-
-// console.log('Axios baseURL =', apiRequest.defaults.baseURL);
 
 export default apiRequest;
