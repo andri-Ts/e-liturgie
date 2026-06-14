e2870b3110ddc1029ef5b341c00de6f347b278b2
Po Bidau <pobidau@protonmail.com>
2026-06-14 23:33:31 +0200

    style; onglet spec

--- src/utils/getLecturesFallBackUrl.js
+++ src/utils/getLecturesFallBackUrl.js
@@ -1,7 +1,12 @@
+/**
+ * Génère le lien vers les lectures du jour sur Katolika.org
+ * lorsque l'API ne retourne pas les textes.
+ */
+
 export function getLecturesFallBackUrl(date) {
   if (!date) return null;
 
   const isoDate = new Date(date).toISOString().split('T')[0];
 
-  return `https://www.aelf.org/${isoDate}/romain/messe`;
+  return `https://katolika.org/katolika/soronamasina/daty/${isoDate}`;
 }
